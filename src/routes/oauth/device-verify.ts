7a156f5d24ff8630555bd62a3de8b8f20fe4bfcf
Kieran Klukas <kieran@dunkirk.sh>
2026-08-01 01:46:12 -0400

    chore: fix device page

--- src/routes/oauth/device-verify.ts
+++ src/routes/oauth/device-verify.ts
@@ -115,6 +115,7 @@ function devicePage(title: string, body: string, status = 200): Response {
 	<link rel="preconnect" href="https://fonts.googleapis.com">
 	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 	<link href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@300..700&display=swap" rel="stylesheet">
+	<link rel="stylesheet" href="/ds/tokens.css">
 	<style>${BASE_STYLES}${DEVICE_STYLES}</style>
 </head>
 <body>
